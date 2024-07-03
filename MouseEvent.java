5fd2d4f1c4e0c8574fee901a33be6be888c43478
ahask93 <ahask93@gmail.com>
2024-07-03 16:37:36 -0400

    sending code for evaluation

---
+++ MouseEvent.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class MouseEvent extends Application {
+    
+}
